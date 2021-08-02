3272cdf33d25d2f94fe941da145570a8a9ad5a50
miayang0513 <a16168972@gmail.com>
2021-08-02 15:42:32 +0800

    feat: add middleware of serialize user

--- src/mocks/resolvers/mockUser.js
+++ src/mocks/resolvers/mockUser.js
@@ -1,4 +1,15 @@
-const initialState = {
+import users from '@/mocks/seeds/user'
+
+const deepClone = (data) => JSON.parse(JSON.stringify(data))
+
+const errorState = {
+  message: {
+    title: '',
+    content: ''
+  }
+}
+
+const successState = {
   success: true,
   message: {
     title: '',
@@ -7,118 +18,118 @@ const initialState = {
   result: {}
 }
 
-const deepClone = (data) => JSON.parse(JSON.stringify(data))
+export default {
+  generalLogin: (req, res, ctx) => {
+    const { email } = req.body
 
-const users = [
-  {
-    lastName: 'Mia',
-    firstName: 'Yang',
-    displayName: 'Mia Yang',
-    avatar: 'https://picsum.photos/200',
-    email: 'mia.yang@frontier.cool',
-    isVerify: true, // 是否驗證
-    locale: 'zh-TW',
-    isOldUser: false
-  },
-  {
-    lastName: 'Old',
-    firstName: 'User',
-    displayName: 'Old User',
-    avatar: 'https://picsum.photos/200',
-    email: 'oldUser@frontier.cool',
-    isVerify: true, // 是否驗證
-    locale: 'zh-TW',
-    isOldUser: true
-  }
-]
+    const user = users.find(user => user.email === email)
+    const response = deepClone(successState)
 
-const generalLogin = (req, res, ctx) => {
-  const { email } = req.body
-
-  const user = users.find(user => user.email === email)
-  const response = deepClone(initialState)
-
-  if (user === undefined) {
-    response.success = false
-    response.message.content = '帳號或密碼錯誤'
-  } else {
-    response.result = {
-      isOldUser: user.isOldUser,
-      accessToken: 'access',
-      refreshToken: 'refresh'
+    if (user === undefined) {
+      response.success = false
+      response.message.content = '帳號或密碼錯誤'
+    } else {
+      response.result.isOldUser = user.isOldUser
+      sessionStorage.setItem('accessToken', email)
+      sessionStorage.setItem('refreshToken', email)
     }
-  }
 
-  return res(
-    ctx.status(200),
-    ctx.json(response)
-  )
-}
-
-const changePassword = (req, res, ctx) => {
-  const response = deepClone(initialState)
-  return res(
-    ctx.status(200),
-    ctx.json(response)
-  )
-}
-
-const sendForgotPasswordEmail = (req, res, ctx) => {
-  const response = deepClone(initialState)
-  return res(
-    ctx.status(200),
-    ctx.json(response)
-  )
-}
-
-const verifyForgotPasswordCode = (req, res, ctx) => {
-  const { verifyCode } = req.body
-
-  const response = deepClone(initialState)
-
-  if (verifyCode === '012345') {
-    response.result = {
-      verifyToken: 'verifyToken'
-    }
-  } else if (verifyCode === '543210') {
-    response.success = false
-    response.message.content = '驗證碼過期'
-  } else {
-    response.success = false
-    response.message.content = '驗證碼錯誤'
-  }
-
-  return res(
-    ctx.status(200),
-    ctx.json(response)
-  )
-}
-
-const resetPassword = (req, res, ctx) => {
-  const { verifyToken } = req.body
-
-  const response = deepClone(initialState)
-
-  if (verifyToken !== 'verifyToken') {
     return res(
-      ctx.status(400),
-      ctx.json({
-        message: {
-          content: '驗證 Token 錯誤'
-        }
-      })
+      ctx.status(200),
+      ctx.json(response)
     )
-  }
+  },
+  getUser: (req, res, ctx) => {
+    const response = deepClone(successState)
+    delete req.user.organizationList
+    response.result.user = req.user
 
-  response.result = {
-    accessToken: 'access',
-    refreshToken: 'refresh'
-  }
+    return res(
+      ctx.status(200),
+      ctx.json(response)
+    )
+  },
+  getUserOrgList: (req, res, ctx) => {
+    const response = deepClone(successState)
+    response.result = {
+      user: {
+        organizationList: req.user.organizationList
+      }
+    }
+    return res(
+      ctx.status(200),
+      ctx.json(response)
+    )
+  },
+  checkEmailExist: (req, res, ctx) => {
+    const response = deepClone(successState)
 
-  return res(
-    ctx.status(200),
-    ctx.json(response)
-  )
+    const { email } = req.body
+    const isExist = (/exist/ig).test(email)
+
+    response.result.isExist = isExist
+
+    return res(
+      ctx.status(200),
+      ctx.json(response)
+    )
+  },
+  changePassword: (req, res, ctx) => {
+    const response = deepClone(successState)
+    return res(
+      ctx.status(200),
+      ctx.json(response)
+    )
+  },
+  sendForgotPasswordEmail: (req, res, ctx) => {
+    const response = deepClone(successState)
+    return res(
+      ctx.status(200),
+      ctx.json(response)
+    )
+  },
+  verifyForgotPasswordCode: (req, res, ctx) => {
+    const { verifyCode } = req.body
+
+    const response = deepClone(successState)
+
+    if (verifyCode === '012345') {
+      response.result = {
+        verifyToken: 'verifyToken'
+      }
+    } else if (verifyCode === '543210') {
+      response.success = false
+      response.message.content = '驗證碼過期'
+    } else {
+      response.success = false
+      response.message.content = '驗證碼錯誤'
+    }
+
+    return res(
+      ctx.status(200),
+      ctx.json(response)
+    )
+  },
+  resetPassword: (req, res, ctx) => {
+    const { verifyToken } = req.body
+
+    if (verifyToken !== 'verifyToken') {
+      const response = deepClone(errorState)
+      response.message.content = '驗證 Token 錯誤'
+      return res(
+        ctx.status(400),
+        ctx.json(response)
+      )
+    } else {
+      const response = deepClone(successState)
+      response.result = {
+        accessToken: 'access',
+        refreshToken: 'refresh'
+      }
+      return res(
+        ctx.status(200),
+        ctx.json(response)
+      )
+    }
+  }
 }
-
-export { generalLogin, changePassword, sendForgotPasswordEmail, verifyForgotPasswordCode, resetPassword }
