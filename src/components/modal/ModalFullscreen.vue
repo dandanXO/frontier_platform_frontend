0647f5bf5c0de78a065091e99f79cba1b6fed0e1
邱于臻 <walle@frontier.cool>
2021-10-21 07:47:12 +0000

    merge material

--- src/components/modal/ModalFullscreen.vue
+++ src/components/modal/ModalFullscreen.vue
@@ -1,22 +1,29 @@
 <template lang="pug">
 div(class="fixed inset-0 z-index:modal bg-black-0")
-  component(:is="component" class="h-screen overflow-y-auto")
+  component(:is="component" v-bind="properties" class="h-screen overflow-y-auto")
 </template>
 
 <script>
 import MaterialUpload from '@/components/assets/material/edit/MaterialUpload.vue'
+import MaterialMerge from '@/components/assets/material/MaterialMerge'
+import MaterialMergePreview from '@/components/assets/material/MaterialMergePreview'
 import MaterialUploadSuccess from '@/components/assets/material/edit/MaterialUploadSuccess.vue'
 
 export default {
   name: 'ModalFullscreen',
   components: {
     MaterialUpload,
+    MaterialMerge,
+    MaterialMergePreview,
     MaterialUploadSuccess
   },
   props: {
     component: {
       type: String,
       required: true
+    },
+    properties: {
+      type: Object
     }
   }
 }
