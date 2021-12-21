75425c1f2d26bf03e81c0cd345df4b1261465fbc
miayang0513 <a16168972@gmail.com>
2021-12-21 14:02:39 +0800

    fix: T0313 multi-select behavior

--- src/components/assets/material/list/RowItemImg.vue
+++ src/components/assets/material/list/RowItemImg.vue
@@ -3,22 +3,20 @@ div(class="w-full min-w-42.5 max-w-67.5")
   div(class="pb-2.5 flex justify-between")
     div(class="font-bold text-primary line-clamp-1") {{material.materialNo}}
     svg-icon(:iconName="statusIconName" size="24" class="text-primary")
-  div(
-    class="relative aspect-ratio rounded bg-black-200 border-block-400 bg-cover mb-2.5 overflow-hidden"
-    :class="{'border': neverScanBefore }"
-    @mouseenter="active = true"
-    @mouseleave="active = false"
-  )
-    img(:src="currentCoverImg" class="w-full h-full")
-    input-checkbox(
-      v-if="active || checked"
-      v-model:inputValue="innerSelectedList"
-      :value="JSON.stringify(material)"
-      class="absolute z-10 top-3 left-3 cursor-pointer"
-      iconColor="text-black-0"
-    )
-    div(v-if="active" class="absolute z-9 inset-0 w-full h-full rounded bg-opacity-70 bg-black-900" @click.stop="goToAssetMaterialDetail(material)")
-  div(class="h-6.5 text-primary text-body1 line-clamp-1 line-height-1.6") {{material.description}}
+  div(class="w-full relative aspect-ratio" @mouseenter="isHover = true" @mouseleave="isHover = false")
+    div(class="w-full h-full rounded-md overflow-hidden bg-cover" :class="{'border': neverScanBefore }")
+      img(:src="currentCoverImg" class="w-full h-full")
+    div(v-if="isHover" class="absolute z-9 inset-0 w-full h-full rounded bg-opacity-70 bg-black-900" @click.stop="goToAssetMaterialDetail(material)")
+    div(v-if="isHover || haveSelectedMoreThanOne" class="absolute z-10 inset-0 w-full h-12")
+      div(class="bg-linear w-full h-full rounded-t-md")
+      input-checkbox(
+        v-model:inputValue="innerSelectedList"
+        :value="JSON.stringify(material)"
+        class="absolute top-3 left-3 cursor-pointer"
+        iconColor="text-black-0"
+        uncheckColor="text-black-0"
+      )
+  div(class="h-6.5 text-primary text-body1 line-clamp-1 line-height-1.6 mt-2.5") {{material.description}}
 </template>
 
 <script>
@@ -39,7 +37,7 @@ export default {
   },
   setup (props, { emit }) {
     const { goToAssetMaterialDetail } = useNavigation()
-    const active = ref(false)
+    const isHover = ref(false)
     const { currentCoverImg, neverScanBefore, statusIconName } = useMaterial(props.material)
 
     const innerSelectedList = computed({
@@ -47,15 +45,15 @@ export default {
       set: (v) => emit('update:selectedList', v)
     })
 
-    const checked = computed(() => props.selectedList.includes(JSON.stringify(props.material)))
+    const haveSelectedMoreThanOne = computed(() => props.selectedList.length > 0)
 
     return {
       innerSelectedList,
       statusIconName,
       currentCoverImg,
       neverScanBefore,
-      active,
-      checked,
+      isHover,
+      haveSelectedMoreThanOne,
       goToAssetMaterialDetail
     }
   }
