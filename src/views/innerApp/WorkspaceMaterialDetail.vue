61911ce310f3aed9887b59be59883b1a9d6b491e
miayang0513 <a16168972@gmail.com>
2021-12-23 17:34:52 +0800

    fix: T0352 & T0348 text line clamp issue

--- src/views/innerApp/WorkspaceMaterialDetail.vue
+++ src/views/innerApp/WorkspaceMaterialDetail.vue
@@ -6,7 +6,7 @@ div(class="w-full h-full flex justify-center")
       div(class="flex items-center pb-2")
         h5(class="text-h5 text-primary font-bold line-clamp-1 pr-3") {{`${material.materialNo} ${material.description}`}}
         svg-icon(iconName="create" class="text-black-700 cursor-pointer" size="24" @click="editMaterial.func(material)")
-      p(class="text-caption text-black-700") {{$t('EE0014')}} : {{new Date(material.updateDate).toLocaleString().replace(', ', ' at ')}}
+      p(class="text-caption text-black-700") {{$t('EE0014')}} : {{new Date(material.updateDate * 1000).toLocaleString().replace(', ', ' at ')}}
     material-detail-internal(:material="material")
 </template>
 
