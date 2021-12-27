1e024da4ab03c59abf9e051c44a409c8b7617bf6
miayang0513 <a16168972@gmail.com>
2021-12-27 12:27:48 +0800

    fix[T0306]: show trend board in collection detail

--- src/components/workspace/ModalCollectionDetail.vue
+++ src/components/workspace/ModalCollectionDetail.vue
@@ -2,7 +2,8 @@
 div(class="w-161.5")
   div(class="border-t border-b border-black-400 px-8")
     div(class="my-5 w-97.5 h-69 mx-auto bg-black-200 flex items-center justify-center")
-      img(v-if="workspaceCollection.trendBoardCoverImg" :src="workspaceCollection.trendBoardCoverImg")
+      a(v-if="workspaceCollection.trendBoardCoverImg" :href="workspaceCollection.trendBoardUrl" target="_blank" class="w-full h-full")
+        img(:src="workspaceCollection.trendBoardCoverImg" class="w-full h-full object-contain")
       p(v-else class="text-body2 text-black-400") {{$t('FF0007')}}
     div(class="pb-5" class="text-body2 text-primary line-height-1.6")
       overlay-scrollbar-container(v-if="workspaceCollection.description" class="max-h-27.5") {{workspaceCollection.description}}
