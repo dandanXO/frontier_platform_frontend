330e0854740f57ce14799788d0460a646d7de25a
Walle <walle@frontier.cool>
2022-01-18 11:28:40 +0800

    feat: cropper image uploading modal adjustment

--- src/components/management/ModalUploadLogo.vue
+++ src/components/management/ModalUploadLogo.vue
@@ -1,26 +1,25 @@
 <template lang="pug">
-div(class="w-120 border-t border-black-400")
-  div(class="w-full flex justify-center items-center border-b border-black-400 overflow-hidden")
-    div(class="w-full h-full flex justify-center items-center py-15 relative")
-      div(
-        v-if="!isUploading && !haveUploadedImage"
-        class="flex flex-col"
-        @drop.stop.prevent="imageOperator.onDrop($event)"
-        @dragover.prevent
-        @dragenter.prevent
-      )
-        btn(size="md" type="secondary" class="h-10 mb-2" @click="uploadImg") {{$t('BB0035') }}
-        span(class="text-body2 font-bold mb-2 text-black-500") {{$t('BB0033')}}
-        span(class="text-body2 mb-2 text-black-500") {{$t('BB0034')}}
-        span(class="text-body2 mb-2 text-black-500") {{$t('BB0059')}}
-        span(class="text-body2 mb-2 text-black-500") {{$t('BB0060')}}
-      svg-icon(
-        v-else-if="isUploading"
-        iconName="loading"
-        size="100"
-        class="justify-self-end cursor-pointer text-brand-dark"
-      )
-      img(v-else class="w-50 h-50" :src="orgLogo")
+div(class="w-86 h-136 border-t border-black-400 flex flex-col")
+  div(class="w-full h-full flex justify-center items-center px-11")
+    div(
+      v-if="!isUploading && !haveUploadedImage"
+      class="flex flex-col"
+      @drop.stop.prevent="imageOperator.onDrop($event)"
+      @dragover.prevent
+      @dragenter.prevent
+    )
+      btn(size="md" type="secondary" class="h-10 mb-4" @click="uploadImg") {{$t('BB0035') }}
+      span(class="text-body2 font-bold mb-2 text-black-500") {{$t('BB0033')}}
+      span(class="text-body2 mb-2 text-black-500") {{$t('BB0034')}}
+      span(class="text-body2 mb-2 text-black-500") {{$t('BB0059')}}
+      span(class="text-body2 mb-2 text-black-500") {{$t('BB0060')}}
+    svg-icon(
+      v-else-if="isUploading"
+      iconName="loading"
+      size="100"
+      class="justify-self-end cursor-pointer text-brand-dark"
+    )
+    img(v-else class="w-50 h-50" :src="orgLogo")
   div(class="h-25 flex justify-center items-center")
     div(v-if="!isUploading && haveUploadedImage" class="grid grid-cols-2 gap-x-3")
       btn(size="md" type="secondary" @click="removeLogo") {{$t('UU0016') }}
