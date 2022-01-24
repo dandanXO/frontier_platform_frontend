1c3ad60286e4324ba80dbc1ce082654b0d4df039
miayang0513 <a16168972@gmail.com>
2022-01-24 16:18:08 +0800

    fix[T0525]: textoverflow on share msg

--- src/components/receivedShare/ModalReceivedShareMessage.vue
+++ src/components/receivedShare/ModalReceivedShareMessage.vue
@@ -3,7 +3,7 @@ div(class="w-132.5")
   div(class="border-t border-black-400 py-10 px-8 text-center text-body2 line-height-1.6 text-primary")
     p(v-if="!share.message") {{$t('RR0147')}}
     overlay-scrollbar-container(v-else class="max-h-116.5")
-      p {{share.message}}
+      p(class="break-words") {{share.message}}
 </template>
 
 <script>
