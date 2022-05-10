a7e80a1859e606c9c69e8ef8e43d24f3b8b4540d
miayang0513 <a16168972@gmail.com>
2022-05-10 15:48:19 +0800

    refactor: make reusable component Tabs

--- src/components/layout/materialDetail/MaterialDetailInternal.vue
+++ src/components/layout/materialDetail/MaterialDetailInternal.vue
@@ -6,85 +6,82 @@ div
       block-material-specification(:material="material")
       block-material-pantone(:pantoneList="material.pantoneList")
       block-material-internal-u3m-status(:material="material")
-  div(class="pt-20 flex flex-col gap-y-10")
-    div(class="w-full grid grid-flow-col gap-x-5 justify-start border-b border-black-400")
-      div(v-for="tab in tabList" class="cursor-pointer" @click="currentTab = tab.id")
-        p(class="pb-2 text-body1" :class="[tab.id === currentTab ? 'border-b-4 border-brand text-primary font-bold' : 'text-black-600']" ) {{ tab.name }}
-    div
-      template(v-if="currentTab === TAB.TAGS")
-        div
-          p(class="pb-3 text-body2 font-bold text-primary") {{ $t("RR0027") }}
-          div(class="flex flex-wrap gap-x-2 gap-y-3")
-            div(v-for="tag in material.publicTagList" class="px-3 h-8 flex items-center bg-primary-thin rounded text-body2 text-primary") {{ tag }}
-        div(class="pt-7")
-          p(class="pb-3 text-body2 font-bold text-primary") {{ $t("RR0071") }}
-          div(class="flex flex-wrap gap-x-2 gap-y-3")
-            div(v-for="tag in material.aiTagList" class="px-3 h-8 flex items-center bg-primary-thin rounded text-body2 text-primary") {{ tag }}
-        div(class="mt-10 rounded-md bg-black-100 px-5 py-7.5")
-          h6(class="text-h6 font-bold text-black-600") {{ $t("EE0026") }}
-          div(class="pt-7.5")
-            p(class="pb-3 text-body2 font-bold text-primary") {{ $t("RR0028") }}
-            div(class="flex flex-wrap gap-x-2 gap-y-3")
-              div(v-for="tag in material.privateTagList" class="px-3 h-8 flex items-center bg-primary-thin rounded text-body2 text-primary") {{ tag }}
-      template(v-else-if="currentTab === TAB.PRICING")
-        div(class="grid gap-y-5")
-          div(v-for="item in materialPublicPriceInfo" class="text-body2 text-primary grid grid-cols-8")
-            p(class="col-span-3") {{ item.name }}
-            p(class="col-span-5") {{ item.value }}
-        div(class="mt-10 rounded-md bg-black-100 p-7.5")
-          h6(class="text-h6 font-bold text-black-600") {{ $t("EE0026") }}
-          div(class="pt-7.5")
-            div(class="grid gap-y-5")
-              div(v-for="item in materialPrivatePriceInfo" class="text-body2 text-primary grid grid-cols-8")
-                p(class="col-span-3") {{ item.name }}
-                p(class="col-span-5") {{ item.value }}
-      template(v-else-if="currentTab === TAB.INVENTORY")
-        div(class="rounded-md bg-black-100 p-7.5 flex flex-col gap-y-17.5")
+  tabs(:tabList="tabList" keyField="id" class="pt-20")
+    template(#default="{ currentTab }")
+      div(class="pt-10")
+        template(v-if="currentTab === TAB.TAGS")
           div
+            p(class="pb-3 text-body2 font-bold text-primary") {{ $t("RR0027") }}
+            div(class="flex flex-wrap gap-x-2 gap-y-3")
+              div(v-for="tag in material.publicTagList" class="px-3 h-8 flex items-center bg-primary-thin rounded text-body2 text-primary") {{ tag }}
+          div(class="pt-7")
+            p(class="pb-3 text-body2 font-bold text-primary") {{ $t("RR0071") }}
+            div(class="flex flex-wrap gap-x-2 gap-y-3")
+              div(v-for="tag in material.aiTagList" class="px-3 h-8 flex items-center bg-primary-thin rounded text-body2 text-primary") {{ tag }}
+          div(class="mt-10 rounded-md bg-black-100 px-5 py-7.5")
+            h6(class="text-h6 font-bold text-black-600") {{ $t("EE0026") }}
+            div(class="pt-7.5")
+              p(class="pb-3 text-body2 font-bold text-primary") {{ $t("RR0028") }}
+              div(class="flex flex-wrap gap-x-2 gap-y-3")
+                div(v-for="tag in material.privateTagList" class="px-3 h-8 flex items-center bg-primary-thin rounded text-body2 text-primary") {{ tag }}
+        template(v-else-if="currentTab === TAB.PRICING")
+          div(class="grid gap-y-5")
+            div(v-for="item in materialPublicPriceInfo" class="text-body2 text-primary grid grid-cols-8")
+              p(class="col-span-3") {{ item.name }}
+              p(class="col-span-5") {{ item.value }}
+          div(class="mt-10 rounded-md bg-black-100 p-7.5")
             h6(class="text-h6 font-bold text-black-600") {{ $t("EE0026") }}
             div(class="pt-7.5")
               div(class="grid gap-y-5")
-                div(v-for="item in materialInventoryInfo" class="text-body2 text-primary grid grid-cols-8")
+                div(v-for="item in materialPrivatePriceInfo" class="text-body2 text-primary grid grid-cols-8")
                   p(class="col-span-3") {{ item.name }}
                   p(class="col-span-5") {{ item.value }}
-          div
-            h6(class="text-h6 font-bold text-black-600") {{ $t("EE0027") }}
-            div(class="pt-7.5 flex flex-col gap-y-7.5")
-              div(class="flex items-center gap-x-11 text-body2 text-primary")
-                p {{ materialInfo.totalInventoryQty.name }}
-                p {{ materialInfo.totalInventoryQty.value }}
-                input-checkbox(
-                  v-model:inputValue="material.isPublicInventory"
-                  :label="$t('EE0028')"
-                  binary
-                  size="20"
-                  disabled
-                )
-              div(class="w-117.5 text-body2 text-primary")
-                div(class="bg-primary-thin w-full h-7.5 grid grid-cols-4 justify-items-center content-center")
-                  p {{ $t("RR0035") }}
-                  p {{ $t("RR0036") }}
-                  p {{ $t("RR0037") }}
-                  p {{ $t("RR0038") }}
-                div(class="divide-y divide-solid divide-primary-thin")
-                  div(v-for="inventory in material.inventoryList" class="h-10.5 grid grid-cols-4 justify-items-center content-center")
-                    p {{ inventory.section }}
-                    p {{ inventory.shelf }}
-                    p {{ inventory.quantity }}
-                    p {{ inventory.unit }}
-      template(v-else-if="currentTab === TAB.SUP")
-        div(class="flex flex-wrap gap-5")
-          attachment-item(
-            v-for="(attachment, index) in attachmentSortedList"
-            :attachmentList="attachmentSortedList"
-            :attachment="attachment"
-            :index="index"
-            isReadOnly
-          )
+        template(v-else-if="currentTab === TAB.INVENTORY")
+          div(class="rounded-md bg-black-100 p-7.5 flex flex-col gap-y-17.5")
+            div
+              h6(class="text-h6 font-bold text-black-600") {{ $t("EE0026") }}
+              div(class="pt-7.5")
+                div(class="grid gap-y-5")
+                  div(v-for="item in materialInventoryInfo" class="text-body2 text-primary grid grid-cols-8")
+                    p(class="col-span-3") {{ item.name }}
+                    p(class="col-span-5") {{ item.value }}
+            div
+              h6(class="text-h6 font-bold text-black-600") {{ $t("EE0027") }}
+              div(class="pt-7.5 flex flex-col gap-y-7.5")
+                div(class="flex items-center gap-x-11 text-body2 text-primary")
+                  p {{ materialInfo.totalInventoryQty.name }}
+                  p {{ materialInfo.totalInventoryQty.value }}
+                  input-checkbox(
+                    v-model:inputValue="material.isPublicInventory"
+                    :label="$t('EE0028')"
+                    binary
+                    size="20"
+                    disabled
+                  )
+                div(class="w-117.5 text-body2 text-primary")
+                  div(class="bg-primary-thin w-full h-7.5 grid grid-cols-4 justify-items-center content-center")
+                    p {{ $t("RR0035") }}
+                    p {{ $t("RR0036") }}
+                    p {{ $t("RR0037") }}
+                    p {{ $t("RR0038") }}
+                  div(class="divide-y divide-solid divide-primary-thin")
+                    div(v-for="inventory in material.inventoryList" class="h-10.5 grid grid-cols-4 justify-items-center content-center")
+                      p {{ inventory.section }}
+                      p {{ inventory.shelf }}
+                      p {{ inventory.quantity }}
+                      p {{ inventory.unit }}
+        template(v-else-if="currentTab === TAB.SUP")
+          div(class="flex flex-wrap gap-5")
+            attachment-item(
+              v-for="(attachment, index) in attachmentSortedList"
+              :attachmentList="attachmentSortedList"
+              :attachment="attachment"
+              :index="index"
+              isReadOnly
+            )
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup>
 import { useI18n } from 'vue-i18n'
 import useMaterial from '@/composables/useMaterial'
 import AttachmentItem from '@/components/assets/material/edit/AttachmentItem.vue'
@@ -93,67 +90,44 @@ import BlockMaterialPantone from '@/components/layout/materialDetail/BlockMateri
 import BlockMaterialSpecification from '@/components/layout/materialDetail/BlockMaterialSpecification.vue'
 import BlockMaterialInternalU3mStatus from '@/components/layout/materialDetail/BlockMaterialInternalU3mStatus.vue'
 
-export default {
-  name: 'MaterialDetailInternal',
-  components: {
-    AttachmentItem,
-    BlockMaterialPreviewImg,
-    BlockMaterialSpecification,
-    BlockMaterialPantone,
-    BlockMaterialInternalU3mStatus
-  },
-  props: {
-    material: {
-      type: Object,
-      required: true
-    }
-  },
-  setup (props) {
-    const { t } = useI18n()
-
-    const TAB = {
-      TAGS: 0,
-      PRICING: 1,
-      INVENTORY: 2,
-      SUP: 3
-    }
-    const tabList = [
-      {
-        name: t('RR0133'),
-        id: TAB.TAGS
-      },
-      {
-        name: t('RR0134'),
-        id: TAB.PRICING
-      },
-      {
-        name: t('RR0135'),
-        id: TAB.INVENTORY
-      },
-      {
-        name: t('RR0136'),
-        id: TAB.SUP
-      }
-    ]
-    const currentTab = ref(tabList[0].id)
-    const {
-      materialInfo,
-      materialInventoryInfo,
-      materialPublicPriceInfo,
-      materialPrivatePriceInfo,
-      attachmentSortedList
-    } = useMaterial(props.material)
-
-    return {
-      attachmentSortedList,
-      tabList,
-      TAB,
-      currentTab,
-      materialInfo,
-      materialInventoryInfo,
-      materialPublicPriceInfo,
-      materialPrivatePriceInfo
-    }
+const props = defineProps({
+  material: {
+    type: Object,
+    required: true
   }
+})
+
+const { t } = useI18n()
+
+const TAB = {
+  TAGS: 0,
+  PRICING: 1,
+  INVENTORY: 2,
+  SUP: 3
 }
+const tabList = [
+  {
+    name: t('RR0133'),
+    id: TAB.TAGS
+  },
+  {
+    name: t('RR0134'),
+    id: TAB.PRICING
+  },
+  {
+    name: t('RR0135'),
+    id: TAB.INVENTORY
+  },
+  {
+    name: t('RR0136'),
+    id: TAB.SUP
+  }
+]
+const {
+  materialInfo,
+  materialInventoryInfo,
+  materialPublicPriceInfo,
+  materialPrivatePriceInfo,
+  attachmentSortedList
+} = useMaterial(props.material)
 </script>
--- src/views/innerApp/Billings.vue
+++ src/views/innerApp/Billings.vue
@@ -1,73 +1,54 @@
 <template lang="pug">
 div(class="px-7.5 pt-7.5 w-full h-full relative")
   p(class="text-body1 font-bold text-primary mb-12.5") {{ $t("OO0004") }}
-  div(class="border-b border-black-400 flex justify-between")
-    div(class="flex gap-x-5")
-      div(v-for="tab in tabList" class="cursor-pointer" @click="toggleTab(tab.path)")
-        p(class="pb-2 text-body1" :class="[tab.path === currentTab ? 'border-b-4 border-brand text-primary font-bold' : 'text-black-600']" ) {{ tab.name }}
-  div(class="relative w-full flex justify-center")
-    plan(v-if="currentTab === 'plan'")
-    payment-detail(v-else-if="currentTab === 'payment'")
-    billing-history(v-else-if="currentTab === 'history'")
+  tabs(:tabList="tabList" @switch="toggleTab($event.path)")
+    template(#default="{ currentTab }")
+      div(class="relative w-full flex justify-center")
+        plan(v-if="currentTab === 'plan'")
+        payment-detail(v-else-if="currentTab === 'payment'")
+        billing-history(v-else-if="currentTab === 'history'")
   notify-bar-inactive(v-if="planStatus.INACTIVE || planStatus.TRANSITION" class="absolute bottom-0 left-0")
 </template>
 
-<script>
-import { ref, computed, reactive, defineAsyncComponent } from 'vue'
+<script setup>
+import { computed, reactive, defineAsyncComponent } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 import { useI18n } from 'vue-i18n'
+const Plan = defineAsyncComponent(() => import('@/components/billings/Plan.vue'))
+const PaymentDetail = defineAsyncComponent(() => import('@/components/billings/PaymentDetail.vue'))
+const BillingHistory = defineAsyncComponent(() => import('@/components/billings/BillingHistory.vue'))
+const NotifyBarInactive = defineAsyncComponent(() => import('@/components/billings/NotifyBarInactive.vue'))
 
-export default {
-  name: 'Billings',
-  components: {
-    Plan: defineAsyncComponent(() => import('@/components/billings/Plan.vue')),
-    PaymentDetail: defineAsyncComponent(() => import('@/components/billings/PaymentDetail.vue')),
-    BillingHistory: defineAsyncComponent(() => import('@/components/billings/BillingHistory.vue')),
-    NotifyBarInactive: defineAsyncComponent(() => import('@/components/billings/NotifyBarInactive.vue'))
-  },
-  props: {
-    tab: {
-      type: String,
-      required: true
-    }
-  },
-  setup (props) {
-    const { t } = useI18n()
-    const route = useRoute()
-    const router = useRouter()
-    const store = useStore()
-    const isLoading = ref(false)
-    const isShowInactiveHint = ref(true)
-    const planStatus = computed(() => store.getters['polling/planStatus'])
-    const currentTab = computed(() => props.tab)
-    const tabList = reactive([
-      {
-        name: t('OO0009'),
-        path: 'plan'
-      },
-      {
-        name: t('OO0010'),
-        path: 'payment'
-      },
-      {
-        name: t('OO0011'),
-        path: 'history'
-      }
-    ])
-
-    const toggleTab = (tab) => {
-      router.push({ name: route.name, params: { tab } })
-    }
-
-    return {
-      currentTab,
-      tabList,
-      toggleTab,
-      isLoading,
-      planStatus,
-      isShowInactiveHint
-    }
+defineProps({
+  tab: {
+    type: String,
+    required: true
   }
+})
+
+const { t } = useI18n()
+const route = useRoute()
+const router = useRouter()
+const store = useStore()
+const planStatus = computed(() => store.getters['polling/planStatus'])
+const tabList = reactive([
+  {
+    name: t('OO0009'),
+    path: 'plan'
+  },
+  {
+    name: t('OO0010'),
+    path: 'payment'
+  },
+  {
+    name: t('OO0011'),
+    path: 'history'
+  }
+])
+
+const toggleTab = (tab) => {
+  router.push({ name: route.name, params: { tab } })
 }
+
 </script>
