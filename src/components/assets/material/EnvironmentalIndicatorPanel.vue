2e5449990d541d8c5d88e726af54068d8f840864
miayang0513 <a16168972@gmail.com>
2022-06-01 15:54:54 +0800

    fix[F22-684]: savings text overflow

--- src/components/assets/material/EnvironmentalIndicatorPanel.vue
+++ src/components/assets/material/EnvironmentalIndicatorPanel.vue
@@ -36,7 +36,7 @@ div(class="flex flex-col")
             div(class="col-span-2 flex items-center")
               div(
                 v-if="property.differenceInPercent != null"
-                class="flex items-center justify-center gap-x-2 rounded px-2"
+                class="h-6 flex items-center justify-center gap-x-2 rounded px-2 whitespace-nowrap text-caption"
                 :class="[property.differenceInPercent > 0 ? 'bg-brand-light text-brand' : 'bg-black-50 text-black-500']"
               )
                 p(class="font-bold") {{ property.differenceInPercent }}% {{ $t('RR0229') }}
