e23da211222b864afe09f15f49c979742b499690
miayang0513 <a16168972@gmail.com>
2022-10-14 02:15:41 +0000

    hotfix/v1.8.2

--- projects/platform/src/components/global/ModalBehavior.vue
+++ projects/platform/src/components/global/ModalBehavior.vue
@@ -5,7 +5,7 @@ div(class="fixed inset-0 z-modal w-screen h-screen bg-grey-900/40 flex justify-c
     div(class="h-8.5 px-5 pb-5 flex justify-between items-start border-b border-grey-100")
       p(class="text-body2 font-bold text-grey-900") {{ header }}
       f-svg-icon(v-if="closable" iconName="clear" size="20" class="cursor-pointer text-grey-600" @click="closeModalBehavior")
-    div(class="px-5 pt-5 pb-10 w-fit max-h-103 box-content overflow-y-auto")
+    f-scrollbar-container(class="px-5 pt-5 pb-10 w-fit max-h-103 box-content")
       slot(name="default")
     div(class="px-5 h-13.5 border-t border-grey-100 flex items-end")
       div(class="w-full flex justify-between")
@@ -16,6 +16,7 @@ div(class="fixed inset-0 z-modal w-screen h-screen bg-grey-900/40 flex justify-c
             v-if="textBtnText !== ''"
             :size="btnSize"
             type="text"
+            :prependIcon="textBtnIcon"
             :disabled="textBtnDisabled"
             data-cy="modal-behavior_text"
             @click="$emit('click:text')"
@@ -24,6 +25,7 @@ div(class="fixed inset-0 z-modal w-screen h-screen bg-grey-900/40 flex justify-c
             v-if="secondaryBtnText !== ''"
             :size="btnSize"
             type="secondary"
+            :prependIcon="secondaryBtnIcon"
             :disabled="secondaryBtnDisabled"
             data-cy="modal-behavior_secondary"
             @click="$emit('click:secondary')"
@@ -32,6 +34,7 @@ div(class="fixed inset-0 z-modal w-screen h-screen bg-grey-900/40 flex justify-c
             v-if="primaryBtnText !== ''"
             :size="btnSize"
             type="primary"
+            :prependIcon="primaryBtnIcon"
             :disabled="primaryBtnDisabled"
             data-cy="modal-behavior_primary"
             @click="$emit('click:primary')"
@@ -66,6 +69,10 @@ defineProps({
     type: String,
     default: ''
   },
+  primaryBtnIcon: {
+    type: String,
+    default: ''
+  },
   primaryBtnDisabled: {
     type: Boolean,
     default: false
@@ -74,11 +81,19 @@ defineProps({
     type: String,
     default: ''
   },
+  secondaryBtnIcon: {
+    type: String,
+    default: ''
+  },
   secondaryBtnDisabled: {
     type: Boolean,
     default: false
   },
   textBtnText: {
+    type: String,
+    default: ''
+  },
+  textBtnIcon: {
     type: String,
     default: ''
   },
