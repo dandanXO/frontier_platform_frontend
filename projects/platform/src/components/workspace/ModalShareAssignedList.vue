4628d34bcd1770ca6c9c3b87dab2092e4497b076
Walle Chiu <walle@frontier.cool>
2022-10-18 08:45:02 +0000

    hotfix/v1.8.4

--- projects/platform/src/components/workspace/ModalShareAssignedList.vue
+++ projects/platform/src/components/workspace/ModalShareAssignedList.vue
@@ -6,34 +6,35 @@ modal-behavior(
   @click:primary="updateAssignedShare"
   @click:text="closeModal"
 )
-  f-scrollbar-container(class="h-100.5 -mx-5 px-5")
-    div(class="grid gap-y-7.5 w-114.5")
-      div(v-for="item in copyShareList" class="flex items-center justify-between h-10.5")
-        div(class="flex items-center gap-x-2.5")
-          img(v-if="item.logo" :src="item.logo" class="w-10 h-10 rounded-full")
-          div(v-else class="w-10 h-10 rounded-full border-grey-200 border border-dashed")
-          div(class="w-86")
-            p(class="text-body2 font-bold w-86 line-clamp-1 mb-2" :class="[item.isRemove ? 'text-grey-200' : 'text-grey-900']") {{ item.name }}
-            div(class="flex items-center gap-x-3")
-              f-input-checkbox(
-                binary
-                v-model:inputValue="item.isCanDownloadU3M"
-                :label="$t('FF0033')"
-                iconSize="20"
-                :disabled="item.isRemove"
-                @update:inputValue="addToUpdateList(item)"
-              )
-              f-input-checkbox(
-                binary
-                v-model:inputValue="item.isCanClone"
-                :label="$t('FF0034')"
-                iconSize="20"
-                :disabled="item.isRemove"
-                @update:inputValue="addToUpdateList(item)"
-              )
-        div
-          p(v-if="item.isRemove" class="text-body2 text-primary-400 cursor-pointer" @click="item.isRemove = false") {{ $t('UU0113') }}
-          p(v-else class="text-body2 text-grey-200 cursor-pointer" @click="item.isRemove = true") {{ $t('FF0060') }}
+  div(class="w-114.5")
+    f-scrollbar-container(class="h-100.5 -mx-5 px-5")
+      div(class="grid gap-y-7.5")
+        div(v-for="item in copyShareList" class="flex items-center justify-between h-10.5")
+          div(class="flex items-center gap-x-2.5")
+            img(v-if="item.logo" :src="item.logo" class="w-10 h-10 rounded-full")
+            div(v-else class="w-10 h-10 rounded-full border-grey-200 border border-dashed")
+            div(class="w-86")
+              p(class="text-body2 font-bold w-86 line-clamp-1 mb-2" :class="[item.isRemove ? 'text-grey-200' : 'text-grey-900']") {{ item.name }}
+              div(class="flex items-center gap-x-3")
+                f-input-checkbox(
+                  binary
+                  v-model:inputValue="item.isCanDownloadU3M"
+                  :label="$t('FF0033')"
+                  iconSize="20"
+                  :disabled="item.isRemove"
+                  @update:inputValue="addToUpdateList(item)"
+                )
+                f-input-checkbox(
+                  binary
+                  v-model:inputValue="item.isCanClone"
+                  :label="$t('FF0034')"
+                  iconSize="20"
+                  :disabled="item.isRemove"
+                  @update:inputValue="addToUpdateList(item)"
+                )
+          div
+            p(v-if="item.isRemove" class="text-body2 text-primary-400 cursor-pointer" @click="item.isRemove = false") {{ $t('UU0113') }}
+            p(v-else class="text-body2 text-grey-200 cursor-pointer" @click="item.isRemove = true") {{ $t('FF0060') }}
 </template>
 
 <script setup>
