b6428a4f386b4bf59fd643b71bd1104ec8f272f1
Raven Pan <raven.pan@frontier.cool>
2023-06-06 21:35:11 +0000

    feat/F22-2181-bookmark-bar-default-bookmark-folder-fixed-on-the-bar
    merge request description:
    
    - Jira Issue: [#2181](https://frontier-cool.atlassian.net/browse/F22-2181)
    - Figma 設計稿：https://www.figma.com/file/xVq2fjbOfHe71SoQbdZoKO/3.-Digital-Thread-(Design)?type=design&node-id=5853-732149&t=4IFiqW41NIDm9Z1n-4
    
    這張 Jira issue 的 scope 只有「顯示 All Threads」，文字呈現的部分跟樣式在 2151 已經調整完畢了，故本次調整僅針對在小螢幕下「TopRight container 遮蔽到 TopLeft container」的情境進行開發。
    
    Screen Shots:
    
    ![image](/uploads/a8b114c3e604cf964367055e7650e339/image.png)
    
    ![image](/uploads/1f55580d6041323b59093e6ced6d1a23/image.png)
    
    ---
    
    commits:
    
    * feat/F22-2181-bookmark-bar-default-bookmark-folder-fixed-on-the-bar
    
    See merge request Frontier.cool/Frontier/Platform/PlatformFrontend!589

--- projects/platform/src/components/threadBoard/ThreadBoardTop.vue
+++ projects/platform/src/components/threadBoard/ThreadBoardTop.vue
@@ -1,154 +1,156 @@
 <template lang="pug">
-div(
-  class="h-13 flex-shrink-0 flex flex-row justify-between items-center px-7.5 border-b border-grey-150"
-)
-  div(class="flex flex-row items-center gap-2")
-    f-svg-icon(iconName="sticker_thread" size="20" class="text-primary-400")
-    span(class="font-bold text-body1 text-grey-900") {{ $t('TT0133') }}
-    span(class="text-body2 text-grey-600") {{ $t('RR0068', { number: threadQty }) }}
-  div(class="flex flex-row items-center gap-2")
-    f-button(
-      v-if="canClearFilterAndSearch"
-      type="special"
-      class="text-grey-400 text-body2"
-      @click="clearAllQuery"
-    ) {{ t('UU0041') }}
-    div(v-if="canClearFilterAndSearch" class="w-px h-6 bg-grey-150")
-    f-button(
-      v-if="threadBoardQuery.onlyShowUnread && unreadThreadQty > 0"
-      type="special"
-      class="text-grey-400 text-body2"
-      @click="markAsAllRead"
-    ) {{ $t('TT0162') }}
-    button(
-      class="px-2.5 h-6 rounded-[20px] flex justify-center items-center gap-x-1.5 outline-none"
-      :class="[threadBoardQuery.onlyShowUnread ? 'bg-primary-0' : 'bg-grey-100']"
-      @click="handleUnreadButtonClick"
+div(class="h-13 flex-shrink-0 px-7.5 border-b border-grey-150")
+  div(class="relative h-full flex flex-row justify-between items-center")
+    div(class="absolute left-0 flex flex-row items-center gap-2")
+      f-svg-icon(iconName="sticker_thread" size="20" class="text-primary-400")
+      span(class="font-bold text-body1 text-grey-900") {{ $t('TT0133') }}
+      span(class="text-body2 text-grey-600") {{ $t('RR0068', { number: threadQty }) }}
+    div(
+      class="relative ml-auto flex flex-row items-center gap-2 pl-4 bg-grey-0"
+      :style="{ 'box-shadow': '-25px 0px 10px rgba(255, 255, 255, 0.9)' }"
     )
-      span(
-        class="text-body2"
-        :class="[threadBoardQuery.onlyShowUnread ? 'text-primary-400' : 'text-grey-800']"
-      ) {{ $t('TT0134') }}
-      span(
-        v-if="showUnreadThreadQty"
-        class="text-caption fond-bold"
-        :class="[threadBoardQuery.onlyShowUnread ? 'text-primary-500 font-bold' : 'text-grey-600']"
-      ) {{ unreadThreadQty }}
-    f-input-text(
-      :class="isLongSearchInput ? 'w-100' : 'w-40'"
-      size="md"
-      :placeholder="isLongSearchInput ? $t('TT0163') : $t('RR0053')"
-      prependIcon="search"
-      :textValue="searchText"
-      @update:textValue="updateSearchText"
-      @focus="isSearchInputFocus = true"
-      @blur="isSearchInputFocus = false"
-    )
-    filter-wrapper(
-      class="w-40"
-      iconName="filter"
-      :displayName="$t('RR0085')"
-      :filterCount="filterCount"
-      :dirty="isFilterDirty"
-    )
-      template(#default="{ collapsePopper }")
-        div(class="w-93.5 rounded shadow-16")
-          div(class="px-4 py-5 flex flex-row items-center justify-between")
-            span(class="text-body2 text-grey-900 font-bold") {{ $t('RR0085') }}
-            f-svg-icon(
-              class="text-grey-600 cursor-pointer"
-              iconName="close"
-              size="24"
-              @click="collapsePopper"
-            )
-          div(class="w-full h-px bg-grey-150")
-          div(class="px-4 py-3.5 flex flex-col gap-3")
-            div(class="flex flex-row items-center gap-2.5")
-              span(class="text-caption text-grey-900 font-bold") {{ $t('TT0062') }}
-              f-button-label(size="sm" @click="clearCreatedByFilter") {{ $t('UU0040') }}
-            div(v-for="item in createdByMenu" :key="item.id")
-              f-input-checkbox(
-                binary
-                :label="item.name"
-                :inputValue="item.checked"
-                @update:inputValue="item.handleInput"
-                iconSize="20"
+      f-button(
+        v-if="canClearFilterAndSearch"
+        type="special"
+        class="text-grey-400 text-body2"
+        @click="clearAllQuery"
+      ) {{ t('UU0041') }}
+      div(v-if="canClearFilterAndSearch" class="w-px h-6 bg-grey-150")
+      f-button(
+        v-if="threadBoardQuery.onlyShowUnread && unreadThreadQty > 0"
+        type="special"
+        class="text-grey-400 text-body2"
+        @click="markAsAllRead"
+      ) {{ $t('TT0162') }}
+      button(
+        class="px-2.5 h-6 rounded-[20px] flex justify-center items-center gap-x-1.5 outline-none"
+        :class="[threadBoardQuery.onlyShowUnread ? 'bg-primary-0' : 'bg-grey-100']"
+        @click="handleUnreadButtonClick"
+      )
+        span(
+          class="text-body2"
+          :class="[threadBoardQuery.onlyShowUnread ? 'text-primary-400' : 'text-grey-800']"
+        ) {{ $t('TT0134') }}
+        span(
+          v-if="showUnreadThreadQty"
+          class="text-caption fond-bold"
+          :class="[threadBoardQuery.onlyShowUnread ? 'text-primary-500 font-bold' : 'text-grey-600']"
+        ) {{ unreadThreadQty }}
+      f-input-text(
+        :class="isLongSearchInput ? 'w-100' : 'w-40'"
+        size="md"
+        :placeholder="isLongSearchInput ? $t('TT0163') : $t('RR0053')"
+        prependIcon="search"
+        :textValue="searchText"
+        @update:textValue="updateSearchText"
+        @focus="isSearchInputFocus = true"
+        @blur="isSearchInputFocus = false"
+      )
+      filter-wrapper(
+        class="w-40"
+        iconName="filter"
+        :displayName="$t('RR0085')"
+        :filterCount="filterCount"
+        :dirty="isFilterDirty"
+      )
+        template(#default="{ collapsePopper }")
+          div(class="w-93.5 rounded shadow-16")
+            div(class="px-4 py-5 flex flex-row items-center justify-between")
+              span(class="text-body2 text-grey-900 font-bold") {{ $t('RR0085') }}
+              f-svg-icon(
+                class="text-grey-600 cursor-pointer"
+                iconName="close"
+                size="24"
+                @click="collapsePopper"
               )
-          div(class="w-full h-px bg-grey-150")
-          div(class="px-4 py-3.5 flex flex-col gap-3")
-            div(class="flex flex-row items-center gap-2.5")
-              span(class="text-caption text-grey-900 font-bold") {{ $t('TT0072') }}
-              f-button-label(size="sm" @click="clearParticipantsFilter") {{ $t('UU0040') }}
-            div(v-for="item in participantFilterItemList" :key="item.id")
-              f-input-checkbox(
-                binary
-                :label="item.name"
-                :inputValue="item.checked"
-                @update:inputValue="item.handleInput"
-                iconSize="20"
-              )
-            f-select-input(
-              v-if="showParticipantSelectInput"
-              size="md"
-              prependIcon="search"
-              :canAddNew="false"
-              :placeholder="$t('TT0166')"
-              :dropdownMenuTree="participantShowMoreMenuTree"
-            )
-            p(
-              v-else
-              class="text-caption text-cyan-400 cursor-pointer"
-              @click="showParticipantSelectInput = true"
-            ) {{ $t('TT0054') }}
-          div(class="w-full h-px bg-grey-150")
-          div(class="px-4 py-3.5 flex flex-col gap-3")
-            div(class="flex flex-row items-center gap-2.5")
-              span(class="text-caption text-grey-900 font-bold") {{ $t('TT0165') }}
-              f-button-label(size="sm" @click="clearStickerTypeFilter") {{ $t('UU0040') }}
-            div(v-for="item in stickerTypeMenu" :key="item.id")
-              f-input-checkbox(
-                binary
-                :label="item.name"
-                :inputValue="item.checked"
-                @update:inputValue="item.handleInput"
-                iconSize="20"
-              )
-          div(class="w-full h-px bg-grey-150")
-          div(class="px-4 py-3.5 flex flex-col gap-3")
-            div(class="flex flex-row items-center gap-2.5")
-              span(class="text-caption text-grey-900 font-bold") {{ $t('RR0065') }}
-              f-button-label(size="sm" @click="clearDateCreatedFilter") {{ $t('UU0040') }}
-            div(class="flex flex-row gap-2")
-              f-input-text(
-                inputType="date"
+            div(class="w-full h-px bg-grey-150")
+            div(class="px-4 py-3.5 flex flex-col gap-3")
+              div(class="flex flex-row items-center gap-2.5")
+                span(class="text-caption text-grey-900 font-bold") {{ $t('TT0062') }}
+                f-button-label(size="sm" @click="clearCreatedByFilter") {{ $t('UU0040') }}
+              div(v-for="item in createdByMenu" :key="item.id")
+                f-input-checkbox(
+                  binary
+                  :label="item.name"
+                  :inputValue="item.checked"
+                  @update:inputValue="item.handleInput"
+                  iconSize="20"
+                )
+            div(class="w-full h-px bg-grey-150")
+            div(class="px-4 py-3.5 flex flex-col gap-3")
+              div(class="flex flex-row items-center gap-2.5")
+                span(class="text-caption text-grey-900 font-bold") {{ $t('TT0072') }}
+                f-button-label(size="sm" @click="clearParticipantsFilter") {{ $t('UU0040') }}
+              div(v-for="item in participantFilterItemList" :key="item.id")
+                f-input-checkbox(
+                  binary
+                  :label="item.name"
+                  :inputValue="item.checked"
+                  @update:inputValue="item.handleInput"
+                  iconSize="20"
+                )
+              f-select-input(
+                v-if="showParticipantSelectInput"
                 size="md"
-                class="flex-1"
-                :textValue="dateCreatedInput.start.value"
-                @update:textValue="dateCreatedInput.start.handleInput"
+                prependIcon="search"
+                :canAddNew="false"
+                :placeholder="$t('TT0166')"
+                :dropdownMenuTree="participantShowMoreMenuTree"
               )
-              span(class="text-body1 text-grey-900") ~
-              f-input-text(
-                inputType="date"
-                size="md"
-                class="flex-1"
-                :textValue="dateCreatedInput.end.value"
-                @update:textValue="dateCreatedInput.end.handleInput"
-              )
-    div(class="w-px h-6 bg-grey-150")
-    f-popper(placement="bottom-end")
-      template(#trigger="{ isSortByMenuExpand }")
-        f-svg-icon(
-          iconName="sortby"
-          size="24"
-          class="transform cursor-pointer text-grey-600 hover:text-primary-400"
-          :class="{ 'text-primary-400': isSortByMenuExpand }"
-        )
-      template(#content)
-        f-contextual-menu(
-          v-model:inputSelectValue="sortBy"
-          :selectMode="CONTEXTUAL_MENU_MODE.SINGLE_NONE_CANCEL"
-          :menuTree="sortByMenuTree"
-        )
+              p(
+                v-else
+                class="text-caption text-cyan-400 cursor-pointer"
+                @click="showParticipantSelectInput = true"
+              ) {{ $t('TT0054') }}
+            div(class="w-full h-px bg-grey-150")
+            div(class="px-4 py-3.5 flex flex-col gap-3")
+              div(class="flex flex-row items-center gap-2.5")
+                span(class="text-caption text-grey-900 font-bold") {{ $t('TT0165') }}
+                f-button-label(size="sm" @click="clearStickerTypeFilter") {{ $t('UU0040') }}
+              div(v-for="item in stickerTypeMenu" :key="item.id")
+                f-input-checkbox(
+                  binary
+                  :label="item.name"
+                  :inputValue="item.checked"
+                  @update:inputValue="item.handleInput"
+                  iconSize="20"
+                )
+            div(class="w-full h-px bg-grey-150")
+            div(class="px-4 py-3.5 flex flex-col gap-3")
+              div(class="flex flex-row items-center gap-2.5")
+                span(class="text-caption text-grey-900 font-bold") {{ $t('RR0065') }}
+                f-button-label(size="sm" @click="clearDateCreatedFilter") {{ $t('UU0040') }}
+              div(class="flex flex-row gap-2")
+                f-input-text(
+                  inputType="date"
+                  size="md"
+                  class="flex-1"
+                  :textValue="dateCreatedInput.start.value"
+                  @update:textValue="dateCreatedInput.start.handleInput"
+                )
+                span(class="text-body1 text-grey-900") ~
+                f-input-text(
+                  inputType="date"
+                  size="md"
+                  class="flex-1"
+                  :textValue="dateCreatedInput.end.value"
+                  @update:textValue="dateCreatedInput.end.handleInput"
+                )
+      div(class="w-px h-6 bg-grey-150")
+      f-popper(placement="bottom-end")
+        template(#trigger="{ isSortByMenuExpand }")
+          f-svg-icon(
+            iconName="sortby"
+            size="24"
+            class="transform cursor-pointer text-grey-600 hover:text-primary-400"
+            :class="{ 'text-primary-400': isSortByMenuExpand }"
+          )
+        template(#content)
+          f-contextual-menu(
+            v-model:inputSelectValue="sortBy"
+            :selectMode="CONTEXTUAL_MENU_MODE.SINGLE_NONE_CANCEL"
+            :menuTree="sortByMenuTree"
+          )
 </template>
 
 <script setup lang="ts">
