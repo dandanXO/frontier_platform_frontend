c90f6e7deebdb76dc3be26b59053806613324321
Raven Pan <raven.pan@frontier.cool>
2023-12-25 09:33:26 +0000

    fix/F22-2866-moodboard-cant-preview-jpg-pdf
    merge request description:
    
    Jira Issue: [#2866](https://frontier-cool.atlassian.net/browse/F22-2866)
    
    1. refactor: delete unused `BlockMaterialAdditionalInfo.vue` file
    2. fix: mood board attachment should show view mode modal after click
    
    ---
    
    commits:
    
    * fix/F22-2866-moodboard-cant-preview-jpg-pdf
    
    See merge request Frontier.cool/Frontier/Platform/PlatformFrontend!705

--- projects/platform/src/components/moodboard/BlockAttachment.vue
+++ projects/platform/src/components/moodboard/BlockAttachment.vue
@@ -10,26 +10,27 @@ div(class="rounded border border-grey-150")
     f-svg-icon(v-else iconName="keyboard_arrow_up" size="24")
   f-scrollbar-container(v-if="isExpand && attachmentList.length > 0" class="max-h-105")
     div(class="flex flex-wrap gap-5 p-6")
-      attachment-item(
+      attachment-card(
         v-for="(attachment, index) in attachmentList"
         :key="attachment.url"
-        :attachmentList="attachmentList"
-        :attachment="attachment"
-        :index="index"
-        :isReadOnly="true"
+        v-bind="mapToCardProps(attachment)"
+        @click="openAttachmentPreview(index)"
       )
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import AttachmentItem from '@/components/common/material/attachment/AttachmentItem.vue'
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+import type { MoodboardAllOfAttachmentList } from '@frontier/platform-web-sdk'
+import AttachmentCard from '../common/material/attachment/AttachmentCard.vue'
+import type { EXTENSION } from '@frontier/constants'
+import type { MaterialViewModeFile } from '@/types'
 
-const props = defineProps({
-  attachmentList: {
-    type: Array,
-    required: true,
-  },
-})
+const store = useStore()
+
+const props = defineProps<{
+  attachmentList: MoodboardAllOfAttachmentList[]
+}>()
 
 const isExpand = ref(false)
 
@@ -37,5 +38,34 @@ const handleExpand = () => {
   if (props.attachmentList.length > 0) {
     isExpand.value = !isExpand.value
   }
+}
+
+const mapToCardProps = (attachment: MoodboardAllOfAttachmentList) => ({
+  thumbnailUrl: attachment.url,
+  originalUrl: attachment.url,
+  extension: attachment.extension as EXTENSION,
+  displayFileName: attachment.displayFileName,
+})
+
+const fileList = computed<MaterialViewModeFile[]>(() =>
+  props.attachmentList.map((a) => ({
+    id: a.attachmentId,
+    originalUrl: a.url,
+    thumbnailUrl: a.url,
+    displayName: a.displayFileName,
+    extension: a.extension as EXTENSION,
+  }))
+)
+
+const openAttachmentPreview = (index: number) => {
+  store.dispatch('helper/pushModal', {
+    component: 'modal-view-mode',
+    properties: {
+      viewModeService: {
+        startIndex: index,
+        fileList,
+      },
+    },
+  })
 }
 </script>
