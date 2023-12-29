ce648f2d8e5f15f56a02cb880fe8ae5f07ae8e14
Raven Pan <raven.pan@frontier.cool>
2023-12-29 08:38:15 +0000

    fix/F22-2887-asset-and-workspace-detail-page-inventory-layout-跑版
    merge request description:
    
    Jira: [#2887](https://frontier-cool.atlassian.net/browse/F22-2887)
    
    ---
    
    commits:
    
    * fix/F22-2887-asset-and-workspace-detail-page-inventory-layout-跑版
    
    See merge request Frontier.cool/Frontier/Platform/PlatformFrontend!718

--- projects/platform/src/components/common/material/detail/internal/MaterialDetailInventoryTable.vue
+++ projects/platform/src/components/common/material/detail/internal/MaterialDetailInventoryTable.vue
@@ -1,23 +1,23 @@
 <template lang="pug">
-table(class="w-fit table-fixed")
-  thead(class="bg-grey-150 w-full")
-    tr(
+div(class="w-fit table-fixed")
+  div(class="bg-grey-150 w-full")
+    div(
       class="h-7.5 grid gap-x-10 content-center px-10 text-left"
       :class="[classGridCols]"
     )
-      th(
+      div(
         v-for="header in headerList"
         :key="header.prop"
         class="font-normal max-w-19 text-body2/1.6 text-grey-800"
       ) {{ header.label }}
-  tbody(class="divide-y divide-solid divide-grey-150")
-    tr(
+  div(class="divide-y divide-solid divide-grey-150")
+    div(
       v-for="(item, index) in itemList"
       :key="`item-${index}`"
-      class="grid items-center gap-x-10 content-center px-10 text-left"
+      class="grid items-center gap-x-10 content-center px-10 py-2.5 text-left"
       :class="[classGridCols]"
     )
-      td(
+      div(
         v-for="header in headerList"
         :key="header.prop"
         class="text-body2/1.6 text-grey-900"
