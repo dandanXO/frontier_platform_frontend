ff6e1b812ab6f7feafc44cd7bf35cb6957744382
Dan Dan <dan@frontier.cool>
2024-04-08 09:13:54 +0000

    fix/F22-2976-upload-title-empty
    merge request description:
    
    update i18n key
    
    ---
    
    commits:
    
    * fix: i18n issue
    
    See merge request Frontier.cool/Frontier/Platform/PlatformFrontend!854

--- projects/platform/src/components/assets/edit/ModalUploadAttachment.vue
+++ projects/platform/src/components/assets/edit/ModalUploadAttachment.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 modal-behavior(
-  :header="$t('MI0102')"
+  :header="$t('RR0297')"
   :primaryBtnText="$t('UU0022')"
   :secondaryBtnText="$t('UU0002')"
   :primaryBtnDisabled="disabledUpload"
