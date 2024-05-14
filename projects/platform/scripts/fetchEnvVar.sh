bcdc61fe75dd95446ec68692b42fb5d2ab067f22
dan <dan@MacBook-Pro.local>
2024-05-14 16:04:00 +0800

    update gitlab token
    # Conflicts:
    #	projects/platform/scripts/deploy.sh

--- projects/platform/scripts/fetchEnvVar.sh
+++ projects/platform/scripts/fetchEnvVar.sh
@@ -1,5 +1,5 @@
 function getVar() {
-  json=$(curl --header "PRIVATE-TOKEN: glpat-aseMRSUiyDo_e6NQcr6a" "https://gitlab.com/api/v4/projects/27800370/variables/$1")
+  json=$(curl --header "PRIVATE-TOKEN: glpat-jih4MynXq-M_A49DXp4z" "https://gitlab.com/api/v4/projects/27800370/variables/$1")
 
   # https://gist.github.com/cjus/1047794
   local var=$(echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w "value")
