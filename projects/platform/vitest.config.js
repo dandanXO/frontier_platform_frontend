09bd63085ad04998bcb3e48c495992655a062cdf
dan <dan@MacBook-Pro.local>
2024-06-19 14:59:11 +0800

    fix vitest config issue

--- projects/platform/vitest.config.js
+++ projects/platform/vitest.config.js
@@ -3,17 +3,34 @@ import { defineConfig, mergeConfig } from 'vitest/config'
 import { fileURLToPath, URL } from 'url'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'happy-dom',
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: 'happy-dom',
       },
-    },
-  })
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      },
+    })
+  )
 )
+
+// export default mergeConfig(
+//   viteConfig,
+//   defineConfig({
+//     test: {
+//       globals: true,
+//       environment: 'happy-dom',
+//     },
+//     resolve: {
+//       alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url)),
+//       },
+//     },
+//   })
+// )
