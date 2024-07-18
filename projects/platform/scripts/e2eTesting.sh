b9aa5397fd89f35df11daaf4ed4341e7954560c4
Muhammad Taufiqulsa'di <taufiqulsadi.muhammad@frontier.cool>
2024-07-18 05:25:49 +0000

    feature/F22-3747-replace-husky-with-cypress-cicd
    merge request description:
    
    **WHAT PROBLEM DOES THIS MR SOLVE?**
    
    * currently we used husky to run e2e automatically every commit we pushed to origin branch, the bad news is we need to wait for the e2e testing completed which takes times to completed.
    
    **How does this MR solve it?**
    
    * this MR will create a bash script and add it to package.json to run cypress via gitlab ci/cd. (will add the .gitlab-ci.yml changes commit in dirrectly in main branch)
    
    ---
    
    commits:
    
    * feat(e2e): [F22-3747] replace husky with normal bash+node script
    
    See merge request Frontier.cool/Frontier/Platform/PlatformFrontend!1077

--- projects/platform/scripts/e2eTesting.sh
+++ projects/platform/scripts/e2eTesting.sh
@@ -1,15 +1,11 @@
-#!/bin/sh
-. "$(dirname $0)/_/husky.sh"
-
 folders=($(git diff HEAD~1 --name-only | xargs dirname | sort | uniq))
 isDisabled=true
 
 for folder in "${folders[@]}"
 do
   if [[ $folder != *"packages/i18n/locales"* ]];then
-    npx start-server-and-test dev:platform 8080 test:e2e:normal
+    npx start-server-and-test "npm run serve --mode=${1}" 8080 cy:run
     break
   fi
-
-   # or do whatever with individual element of the array
 done
+
