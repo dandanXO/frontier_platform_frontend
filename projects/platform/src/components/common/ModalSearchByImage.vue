a82f2ec9501999b503e8fbdd042e73decd7f882f
Muhammad Taufiqulsadi <mtaufiqulsadi@gmail.com>
2025-03-05 16:14:26 +0800

    feat(search-by-image): [F22-4470] positive flow done

--- projects/platform/src/components/common/ModalSearchByImage.vue
+++ projects/platform/src/components/common/ModalSearchByImage.vue
@@ -4,10 +4,18 @@ div(
 )
   f-svg-icon(iconName="upload" size="42" class="text-brand-solid")
   p(class="text-base text-primary-inverse") {{ $t('RR0484') }}
-  f-button(size="md" type="secondary") {{ $t('DD0038') }}
+  f-button(size="md" type="secondary" @click="chooseFile") {{ $t('DD0038') }}
+ul(class="list-disc pl-5")
+  li {{ $t('RR0145') }} {{ bytesToSize(fileSizeMaxLimit) }}
+  li {{ $t('RR0485') }} {{ acceptTypes.join(', ') }}
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+import { bytesToSize, FileOperator } from '@frontier/lib'
+import { Extension } from '@frontier/platform-web-sdk'
+
 interface Props {
   title: string
   description: string
@@ -17,4 +25,17 @@ interface Props {
   secondaryBtnHandler: VoidFunction
 }
 defineProps<Props>()
+
+const acceptTypes: Extension[] = [Extension.JPG, Extension.PNG, Extension.JPEG]
+const fileName = ref('')
+const binaryFile = ref<File>()
+const fileSizeMaxLimit = 10 * 1024 * 1024 // 10MB
+const fileOperator = new FileOperator(acceptTypes, fileSizeMaxLimit)
+
+const chooseFile = () => fileOperator.upload(false)
+
+fileOperator.on('finish', (file: File) => {
+  binaryFile.value = file
+  fileName.value = file.name
+})
 </script>
