4052aee60c4b6948b32a8055fea7d7d31c4954a6
Muhammad Taufiqulsadi <mtaufiqulsadi@gmail.com>
2025-03-11 12:14:25 +0800

    feat(search-by-image): [F22-4470] fix focus shadow

--- packages/ui-component/src/FSearchBar/FSearchBar.vue
+++ packages/ui-component/src/FSearchBar/FSearchBar.vue
@@ -1,10 +1,8 @@
 <template lang="pug">
-div(
-  class="justify-self-center h-10 bg-primary border border-primary-border rounded flex items-center overflow-hidden"
-  :class="dynamicWidth"
-)
+div(class="justify-self-center bg-primary flex items-center" :class="[dynamicWidth]")
   div(
-    class="flex-grow flex items-center pl-3 pr-2 gap-2 text-primary-inverse border-r border-primary-border h-full"
+    class="flex-grow flex items-center pl-3 pr-2 gap-2 text-primary-inverse h-full border border-primary-border rounded-l"
+    :class="{ 'shadow-focus': isFocusedInput }"
     data-cy="search-box"
   )
     f-svg-icon(class="pr-1" size="24" iconName="search")
@@ -13,6 +11,8 @@ div(
       :value="keyword"
       :placeholder="placeholder ?? $t('RR0053')"
       @input="$emit('typing', $event)"
+      @focus="isFocusedInput = true"
+      @blur="isFocusedInput = false"
       @keydown.enter="$emit('search')"
       class="placeholder:text-grey-250 placeholder:overflow-visible flex-grow w-full outline-none bg-transparent overflow-hidden text-grey-900 text-body1 disabled:text-grey-600"
     )
@@ -24,7 +24,7 @@ div(
       @click="$emit('clear')"
     )
   div(
-    class="flex items-center px-4 py-2 hover:bg-primary-hover cursor-pointer"
+    class="flex items-center px-4 py-2 hover:bg-primary-hover cursor-pointer border-r border-t border-b border-primary-border rounded-r"
     @click="$emit('clickRightIcon')"
   )
     f-svg-icon(
@@ -43,10 +43,11 @@ export default {
 </script>
 
 <script lang="ts" setup>
+import { computed, ref } from 'vue'
 import { useBreakpoints } from '@frontier/lib'
-import { computed } from 'vue'
-const { isDesktop } = useBreakpoints()
 
+const { isDesktop } = useBreakpoints()
+const isFocusedInput = ref(false)
 const dynamicWidth = computed(() => {
   return isDesktop.value ? 'max-w-192 min-w-112' : 'max-w-[370px] min-w-80'
 })
@@ -65,3 +66,10 @@ defineEmits<{
   (e: 'search'): void
 }>()
 </script>
+
+<style lang="scss" scoped>
+.shadow-focus {
+  box-shadow: 0px 0px 0px 2px #8addf4;
+  transition: box-shadow 0.2s ease;
+}
+</style>
