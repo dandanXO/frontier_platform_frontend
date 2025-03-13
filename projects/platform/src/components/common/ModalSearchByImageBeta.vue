de4efbccf383d70b8d8a6c6d32adbf5402a033b6
Muhammad Taufiqulsadi <mtaufiqulsadi@gmail.com>
2025-03-13 10:14:39 +0800

    fix i18n

--- projects/platform/src/components/common/ModalSearchByImageBeta.vue
+++ projects/platform/src/components/common/ModalSearchByImageBeta.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
 div(class="flex flex-col gap-6")
   img(src="@/assets/images/search_by_image_beta.png")
-  p(class="text-center text-sm text-primary-inverse") {{ $t('RR0486') }}
+  p(class="text-center text-sm text-primary-inverse") {{ $t('RR0485') }}
 </template>
