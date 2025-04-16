db2b0d8e544bb89e9907b50e4ad6ad9a833e7c27
Muhammad Taufiqulsa'di <taufiqulsadi.muhammad@frontier.cool>
2025-04-16 07:04:21 +0000

    fix/F22-4633-contact-support-not-found
    merge request description:
    
    #### **WHAT PROBLEM DOES THIS MR SOLVE?**
    
    * the email when page not found is incorrect.
    
    #### **How does this MR solve it?**
    
    * change the email to `support@frontier.cool`
    
    **Result**
    
    ![Screen Recording 2025-04-16 at 15.01.13.mov](/uploads/cda8ae4a4582721b1df0bc75cb2d7f0c/Screen_Recording_2025-04-16_at_15.01.13.mov)
    
    ---
    
    commits:
    
    * fix(not-found): [F22-4633] contact support not found
    
    See merge request Frontier.cool/Frontier/Platform/PlatformFrontend!1300

--- projects/platform/src/components/common/ContactSupport.vue
+++ projects/platform/src/components/common/ContactSupport.vue
@@ -2,5 +2,5 @@
 div(class="flex flex-col py-6 px-4 gap-1 bg-info rounded-xl w-9/12")
   span(class="text-secondary-text") {{ $t('UU0166') }}
   span(class="text-link font-semibold cursor-pointer underline") 
-    a(href="mailto:support@textilecloud.com") support@textilecloud.com
+    a(href="mailto:support@frontier.cool") support@frontier.cool
 </template>
