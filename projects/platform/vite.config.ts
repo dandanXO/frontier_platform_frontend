73d98cf951061f795d45ba1b48fca972e9b6e97b
dan <dan@mac-nb-DAN.local>
2025-04-22 16:15:07 +0800

    update vite issue

--- projects/platform/vite.config.ts
+++ projects/platform/vite.config.ts
@@ -60,7 +60,6 @@ export default ({ mode }) => ({
   },
   build: {
     sourcemap: process.env.NODE_ENV === 'development',
-    minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
